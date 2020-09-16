8f7085ed13bf8046f24004014ae2d39c1559fcfa
Shaun Saker <sakershaun@gmail.com>
2020-09-16 08:46:01 +0200

    onWriteApiHealthPing check change exists

--- src/onWriteApiHealthPing/index.ts
+++ src/onWriteApiHealthPing/index.ts
@@ -4,14 +4,16 @@ import { mailer } from "../mailer";
 import { ApiHealth } from "../models";
 
 const onWriteApiHealthPing = functions.firestore.document("api/health").onWrite(async (change) => {
-  const { isAlive } = change.after.data() as ApiHealth;
+  if (change.after.exists) {
+    const { isAlive } = change.after.data() as ApiHealth;
 
-  if (!isAlive) {
-    await mailer({
-      to: EMAIL_USERNAME,
-      subject: "BOT API DOWN",
-      text: "The Bot's REST API is down.",
-    });
+    if (!isAlive) {
+      await mailer({
+        to: EMAIL_USERNAME,
+        subject: "BOT API DOWN",
+        text: "The Bot's REST API is down.",
+      });
+    }
   }
 });
 
