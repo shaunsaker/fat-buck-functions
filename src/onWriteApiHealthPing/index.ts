49619650bf684f25b51f294d192edcb7af60bce0
Shaun Saker <sakershaun@gmail.com>
2020-09-16 08:33:59 +0200

    fix onWriteApiHealthPing document ref

--- src/onWriteApiHealthPing/index.ts
+++ src/onWriteApiHealthPing/index.ts
@@ -3,18 +3,16 @@ import { EMAIL_USERNAME } from "../config";
 import { mailer } from "../mailer";
 import { ApiHealth } from "../models";
 
-const onWriteApiHealthPing = functions.firestore
-  .document("api/health/ping")
-  .onWrite(async (change) => {
-    const { isAlive } = change.after.data() as ApiHealth;
+const onWriteApiHealthPing = functions.firestore.document("api/health").onWrite(async (change) => {
+  const { isAlive } = change.after.data() as ApiHealth;
 
-    if (!isAlive) {
-      await mailer({
-        to: EMAIL_USERNAME,
-        subject: "BOT API DOWN",
-        text: "The Bot's REST API is down.",
-      });
-    }
-  });
+  if (!isAlive) {
+    await mailer({
+      to: EMAIL_USERNAME,
+      subject: "BOT API DOWN",
+      text: "The Bot's REST API is down.",
+    });
+  }
+});
 
 export { onWriteApiHealthPing };
