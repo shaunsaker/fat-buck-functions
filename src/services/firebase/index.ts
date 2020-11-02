6a6fd897367bbb524e0b3b2dce4f0079e850e9e3
Shaun Saker <sakershaun@gmail.com>
2020-11-02 17:01:24 +0200

    fix serviceAccount import

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,9 +1,16 @@
 import * as admin from 'firebase-admin';
+import serviceAccount from './service-account.json';
 
-const serviceAccount = require('./service-account.json');
+// jest can't import json and tsc needs to import it so we add a fallback to require
+const serviceAccountFallback = require('./service-account.json');
+const serviceAccountToUse = serviceAccount || serviceAccountFallback;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    privateKey: serviceAccountToUse.private_key,
+    clientEmail: serviceAccountToUse.client_email,
+    projectId: serviceAccountToUse.project_id,
+  }),
 });
 
 export { admin as firebase };
