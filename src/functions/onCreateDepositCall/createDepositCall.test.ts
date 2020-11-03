4ea4e245367a8cef36602163fce3bd4524858653
Shaun Saker <sakershaun@gmail.com>
2020-11-03 09:35:16 +0200

    update createDepositCall tests

--- src/functions/onCreateDepositCall/createDepositCall.test.ts
+++ src/functions/onCreateDepositCall/createDepositCall.test.ts
@@ -1,6 +1,7 @@
 import { CallableContext } from 'firebase-functions/lib/providers/https';
 import { createDepositCall } from '.';
 import { DepositCallData, DepositStatus } from '../../services/firebase/models';
+import { MOCKED_MOMENT_ISO_STRING } from '../../../__mocks__/moment';
 
 describe('createDepositCall', () => {
   const walletAddress = '12345678';
@@ -22,22 +23,22 @@ describe('createDepositCall', () => {
     expect(saveDepositCall).not.toHaveBeenCalled();
   });
 
-  // it('calls saveDepositCall and returns success', async () => {
-  //   const uid = '1111111';
-  //   const context = {
-  //     auth: {
-  //       uid,
-  //     },
-  //   } as CallableContext;
-  //   const result = await createDepositCall(data, context, saveDepositCall);
-  //   const expectedDepositCallData: DepositCallData = {
-  //     uid,
-  //     date: '',
-  //     walletAddress,
-  //     status: DepositStatus.PENDING,
-  //   };
+  it('calls saveDepositCall and returns success', async () => {
+    const uid = '1111111';
+    const context = {
+      auth: {
+        uid,
+      },
+    } as CallableContext;
+    const result = await createDepositCall(data, context, saveDepositCall);
+    const expectedDepositCallData: DepositCallData = {
+      uid,
+      date: MOCKED_MOMENT_ISO_STRING,
+      walletAddress,
+      status: DepositStatus.PENDING,
+    };
 
-  //   expect(saveDepositCall).toHaveBeenCalledWith(expectedDepositCallData);
-  //   expect(result.success).toEqual(true);
-  // });
+    expect(saveDepositCall).toHaveBeenCalledWith(expectedDepositCallData);
+    expect(result.success).toEqual(true);
+  });
 });
--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 
 export const getDate = (timestamp?: number): string => {
   const date = moment(timestamp).toISOString();
